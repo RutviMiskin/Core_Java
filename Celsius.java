67485e9f542329cfe8959bf365f432d29ab1d944
RutviMiskin <rm.miskin2001@gmail.com>
2022-07-22 12:58:18 +0530

    Activity

--- Celsius.java
+++ Celsius.java
@@ -1,10 +1,12 @@
+//Write a java program to convert fahrenheit to celsius.
+
 class Celsius
 {
 	public static void main(String args[])
 	{
 		float Fahrenheit, Celsius;
 		Fahrenheit=3.4f;
-		Celsius=((Fahrenheit-32)*5/9);
+		Celsius=((Fahrenheit-32)*5/9); //to convert Celsius
 		System.out.println("Temperature in Celsius is:"+Celsius);
 	}
 }
