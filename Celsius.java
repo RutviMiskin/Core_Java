2447c71a6e591fdb46b0d50008e0de34a046af5c
RutviMiskin <rm.miskin2001@gmail.com>
2022-07-22 12:22:50 +0530

    Activity

---
+++ Celsius.java
@@ -0,0 +1,10 @@
+class Celsius
+{
+	public static void main(String args[])
+	{
+		float Fahrenheit, Celsius;
+		Fahrenheit=3.4f;
+		Celsius=((Fahrenheit-32)*5/9);
+		System.out.println("Temperature in Celsius is:"+Celsius);
+	}
+}
